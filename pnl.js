4f826e07ab5c151a59b5c68a029a46a62b71cd49
Benjamin Braner <benjamin.branner@gmail.com>
2024-09-07 17:40:37 -0400

    Descripción de los cambios

--- pnl.js
+++ pnl.js
@@ -1,41 +1,47 @@
-const precioEntradaInput = document.getElementById("precioEntrada");
-const apalancamientoInput = document.getElementById("apalancamiento");
-const cantidadInput = document.getElementById("cantidad");
-const precioSalidaInput = document.getElementById("precioSalida");
+const precioEntradaInput = document.getElementById('precioEntrada')
+const apalancamientoInput = document.getElementById('apalancamiento')
+const cantidadInput = document.getElementById('cantidad')
+const precioSalidaInput = document.getElementById('precioSalida')
 
-const respMargenInicial = document.getElementById("margen-inicial");
-const respGanancia = document.getElementById("ganancia");
-const respRoi = document.getElementById("roi");
+const respMargenInicial = document.getElementById('margen-inicial')
+const respGanancia = document.getElementById('ganancia')
+const respRoi = document.getElementById('roi')
 
-const botonCalcular = document.getElementById("calcularProfit");
-
-
+const botonCalcular = document.getElementById('calcularProfit')
 
 function calcularProfit(precioEntrada, apalancamiento, cantidad, precioSalida) {
-     const margenInicial = cantidad / apalancamiento;
-     const dif = precioSalida - precioEntrada;
-     const profitUSDT = margenInicial * dif;
-     const profitPorcentaje = (profitUSDT / margenInicial) * 100;
-
-     return {
-          margenInicial,
-          profitUSDT,
-          profitPorcentaje,
-     };
+	const margenInicial = cantidad / apalancamiento // Cantidad sin apalancar
+	const porcentajeCambio =
+		((precioSalida - precioEntrada) / precioEntrada) * 100
+	const multiplicadorPorcentaje = porcentajeCambio / 100;
+	const profitUSDT = cantidad * multiplicadorPorcentaje
+	const profitPorcentaje = (profitUSDT / margenInicial) * 100 // Porcentaje de beneficio sobre el margen inicial
+console.log(margenInicial)
+console.log(multiplicadorPorcentaje)
+console.log(profitUSDT)
+console.log(profitPorcentaje)
+	return {
+		margenInicial,
+		profitUSDT,
+		profitPorcentaje
+	}
 }
 
+botonCalcular.addEventListener('click', function (e) {
+	e.preventDefault()
+	const precioEntrada = precioEntradaInput.value
+	const apalancamiento = apalancamientoInput.value
+	const cantidad = cantidadInput.value
+	const precioSalida = precioSalidaInput.value
 
-botonCalcular.addEventListener("click", function (e) {
-     e.preventDefault()
-     const precioEntrada = precioEntradaInput.value;
-     const apalancamiento = apalancamientoInput.value;
-     const cantidad = cantidadInput.value;
-     const precioSalida = precioSalidaInput.value;
+	const { margenInicial, profitUSDT, profitPorcentaje } = calcularProfit(
+		precioEntrada,
+		apalancamiento,
+		cantidad,
+		precioSalida
+	)
 
-     const { margenInicial, profitUSDT, profitPorcentaje } = calcularProfit(precioEntrada, apalancamiento, cantidad, precioSalida);
-
-     respMargenInicial.value = margenInicial + "$";
-     respGanancia.value = profitUSDT + "$";
-     respRoi.value = profitPorcentaje + "%";
-
-});
+	respMargenInicial.value = margenInicial + '$'
+	respGanancia.value = profitUSDT + '$'
+	respRoi.value = profitPorcentaje + '%'
+})
