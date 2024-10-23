939a3da207ef21e555a9483d1fe26972b0d3b42a
Benjamin Braner <benjamin.branner@gmail.com>
2024-10-22 23:53:02 -0400

    arreglos en forex

--- tamañoPosicionForex.js
+++ tamañoPosicionForex.js
@@ -1,35 +1,7 @@
 const precioEntradaInputForex = document.querySelector('.entry-inp-forex');
 const precioStopLossInput = document.querySelector('.sl-inp-forex');
-
+document.querySelector('.risk-inp-forex').value = "0.5";
 // Función para manejar el evento input
-const handleInputChange = (input) => {
-    const currentValue = input.value;
-    // Si el valor actual es vacío, restablecer a '1'
-    if (currentValue === '') {
-        input.value = '1.';
-        return;
-    }
-
-    // Evitar escribir cualquier cosa delante del '1'
-    if (!currentValue.startsWith('1.')) {
-        input.value = '1.'; // Restablecer a '1' si no comienza con '1'
-        return;
-    }
-
-    // Si el valor comienza con '1' y no tiene un punto
-    if (currentValue.startsWith('1.') && !currentValue.includes('.')) {
-        // Añadir el punto antes del segundo carácter
-        input.value = '1.' + currentValue.slice(1);
-    }
-     // Filtrar solo números y el punto
-	input.value = input.value.replace(/[^0-9.]/g, ''); // Permite solo números y punto
-console.log(input.value)
-
-};
-
-// Agrega el evento input a ambos inputs
-precioEntradaInputForex.addEventListener('input', () => handleInputChange(precioEntradaInputForex));
-precioStopLossInput.addEventListener('input', () => handleInputChange(precioStopLossInput));
 
 
 function calcularTamanoPosicion() {
@@ -53,8 +25,8 @@ function calcularTamanoPosicion() {
 
 
      document.querySelector('.risk-dol-forex').value = riesgoDolares;
-     document.querySelector('.tam-pos-dol-forex').value = convertirEscala(tamanoPosicion);
-     document.querySelector('.tam-pos-act-forex').value = tamanoPosicion;
+     document.querySelector('.tam-pos-dol-forex').value = convertirEscala(tamanoPosicion).toFixed(4)
+     document.querySelector('.tam-pos-act-forex').value = tamanoPosicion.toFixed(4);
 
  }
  
